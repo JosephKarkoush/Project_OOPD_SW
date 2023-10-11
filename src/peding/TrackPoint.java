c058d8b90a612f46e566f03a9b4d295b34f21fd4
moham <moham@Mohammed>
2023-10-11 15:53:12 +0200

    skrivit kod i trackpoint

--- src/peding/TrackPoint.java
+++ src/peding/TrackPoint.java
@@ -1,5 +1,15 @@
 package peding;
 
-public class TrackPoint {
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class TrackPoint {
+	List<String>linkedList = new ArrayList<String>();
+	
+	public TrackPoint(String rad) {
+		
+	}
+	
+	
 }
