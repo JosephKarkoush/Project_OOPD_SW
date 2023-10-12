265740df27dde79390fda8526e3d578e1d7ab058
Joseph <Joseph@193.11.62.182>
2023-10-12 13:22:02 +0200

    commit

--- src/peding/Import.java
+++ src/peding/Import.java
@@ -1,5 +1,18 @@
 package peding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Import {
+	List<TrackPoint> trackPointList = new ArrayList<TrackPoint>();
+	
+//	JFileChooser chooser = new JFileChooser();
+//	chooser.showOpenDialog(null);
+//	File file = chooser.getSelectedFile();
+//	String filepath = file.getAbsolutePath());
+	
+	public Import() {
+		
+	}
 
 }
