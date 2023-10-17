d90ebc8c17b75db5d12d752d337c6ad82049e232
moham <moham@193.11.63.204>
2023-10-17 12:52:19 +0200

    Last Gui Version 2023-10-17

--- src/view/ActivityPanel.java
+++ src/view/ActivityPanel.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.sql.RowSetListener;
 import javax.swing.JButton;
@@ -9,13 +10,18 @@ import javax.swing.JPanel;
 
 public class ActivityPanel extends JPanel {
 	DataPanel dataPanel = new DataPanel();
+	
+	JPanel secondButtonPanel = new JPanel();
 	JButton changeName = new JButton("Change name");
 	JComboBox<String> comboActivityList = new JComboBox<>();
 
 	public ActivityPanel() {
 		setLayout(new BorderLayout());
-		add(changeName, BorderLayout.SOUTH);
-		add(comboActivityList, BorderLayout.SOUTH);
+		secondButtonPanel.setLayout(new GridLayout(1,2));
+		secondButtonPanel.add(changeName);
+		secondButtonPanel.add(comboActivityList);
+		
+		add(secondButtonPanel, BorderLayout.SOUTH);
 		add(dataPanel, BorderLayout.CENTER);
 
 	}
--- src/view/DisplayPanel.java
+++ src/view/DisplayPanel.java
@@ -1,6 +1,8 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -9,13 +11,17 @@ import javax.swing.JTabbedPane;
 public class DisplayPanel extends JPanel{
 	
 	GraphPanel graphPanel = new GraphPanel();
+	
+	JPanel buttonPanel = new JPanel();
 	JButton importButton = new JButton("Import");
 	JButton addListButton = new JButton("Add to list");
 	
 	public DisplayPanel() {
 		setLayout(new BorderLayout());
-		add(importButton, BorderLayout.SOUTH);
-		add(addListButton, BorderLayout.SOUTH);
+		buttonPanel.setLayout(new GridLayout(1,2));
+		buttonPanel.add(addListButton);
+		buttonPanel.add(importButton);
+		add(buttonPanel, BorderLayout.SOUTH);
 		add(graphPanel, BorderLayout.CENTER);
 		
 		
