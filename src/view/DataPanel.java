663082ee1a7b660852fb46deb52499c1dd07d1d1
Joseph <Joseph@130.243.2.244>
2023-10-23 11:49:38 +0200

    Changes DataPanel

--- src/view/DataPanel.java
+++ src/view/DataPanel.java
@@ -1,15 +1,20 @@
 package view;
 
+import java.awt.FlowLayout;
+
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-public class DataPanel extends JScrollPane {
+public class DataPanel extends JPanel {
 	
-	JLabel dataInformation = new JLabel();
+	JLabel dataInformation = new JLabel("");
 	
 	public DataPanel() {
+		setLayout(new FlowLayout());
 		add(dataInformation);
-		
+		setVisible(true);
+		setEnabled(true);
 	}
 	
 	public void setText(String str) {
