e0009ed3bbb650e5437516800be917f1274a61aa
moham <moham@193.11.48.241>
2023-11-07 15:28:53 +0100

    Last working veriso

--- src/view/MapGraph.java
+++ src/view/MapGraph.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 import controller.Controller1;
@@ -22,6 +23,8 @@ public class MapGraph extends JPanel {
 	public MapGraph(Controller1 ctr) {
 		this.ctr = ctr;
 		this.trackpoints = ctr.getCurrentActivity().getList();
+		setBackground(Color.WHITE);
+		setBorder(BorderFactory.createTitledBorder("map"));
 	}
 
 	public void paintComponent(Graphics g) {
@@ -47,7 +50,7 @@ public class MapGraph extends JPanel {
 		minLat = Double.parseDouble(trackpoints.get(0).getLatitude());
 		minLon = Double.parseDouble(trackpoints.get(0).getLongitude());
 		maxLat = Double.parseDouble(trackpoints.get(0).getLatitude());
-		maxLat = Double.parseDouble(trackpoints.get(0).getLongitude());
+		maxLon = Double.parseDouble(trackpoints.get(0).getLongitude());
 		System.out.println("Mohammed");
 		// Finds min and max:
 		for (TrackPoint tp : trackpoints) {
@@ -66,14 +69,19 @@ public class MapGraph extends JPanel {
 
 	private int getXPixValue(TrackPoint tp) {
 		int xPix = (int) (((Double.parseDouble(tp.getLongitude()) - minLon) / (maxLon - minLon)) * getWidth());
-		System.out.println("getX");
+		System.out.println(xPix);
 		return xPix;
 	}
 
 	private int getYPixValue(TrackPoint tp) {
 		int yPix = (int) (((Double.parseDouble(tp.getLatitude()) - minLat) / (maxLat - minLat)) * getHeight());
 		yPix = getHeight() - yPix; // To adjust for y-axis going "downwards" in graphics
-		System.out.println("gety");
+		System.out.println(yPix);
+//		for(TrackPoint tc : trackpoints) {
+//			System.out.println(tc.getLatitude());
+//		}
+		
+		
 		return yPix;
 	}
 
